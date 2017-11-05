81aae3de3a20880ed963bb20f6f35fa5a844c315
Jacob Chaffin <jchaffin57@gmail.com>
2017-11-05 04:14:55 -0800

    Initial Commit
    Previous dotemacs repository is cluttered with user-facing ssh and api
    keys in the commit history. Rather than scanning through revision logs
    at the I'm migrating to a new repo.

---
+++ custom/custom.el
@@ -0,0 +1,15 @@
+(setq org-agenda-files '("~/Dropbox/Documents/Courses/FTV_108/org/TODOS.org"
+			 "~/Dropbox/Documents/Courses/PHILO_31/org/TODOS.org"
+			 "~/Dropbox/Documents/Courses/LING_20/org/TODOS.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((eval sh-set-shell "zsh")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F")))))
