748dbcf62f23f0b6ed8a392c9eaffc012e9ba482
root <root@1f66524b34b8>
2017-12-28 08:16:07 +0000

    Refactor Ivy section (literate structure)
    - Enable `org-bullets' outside of a hook.
    - Set `org-agenda-files' to `nil'; should use different custom files
      based on OS and/or computer name.
    - Wrap the menu-bar disable command in the slightly more consice
      `*is-mac' constant.
    - Don't enable  `org-beautify-theme' without a window system.

--- custom/custom.el
+++ custom/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(safe-local-variable-values
    (quote
     ((eval bibtex-set-dialect
