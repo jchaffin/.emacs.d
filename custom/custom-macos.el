f0fa0bc4b3ce33ff606f2f3e5935b317bf31bc0a
Jacob Chaffin <jchaffin@ucla.edu>
2017-12-30 02:05:25 -0800

    Upgrade my Emacs user interface
    Looks like atom beta in this biatch.

--- custom/custom-macos.el
+++ custom/custom-macos.el
@@ -4,27 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((eval bibtex-set-dialect
-	   (quote bibtex))
-     (org-image-actual-width)
-     (eval bibtex-set-dialect
-	   (quote biblatex))
-     (cider-cljs-lein-repl . "(do (user/go) (user/cljs-repl))")
-     (cider-refresh-after-fn . "reloaded.repl/resume")
-     (cider-refresh-before-fn . "reloaded.repl/suspend")
-     (org-ref-default-bibliography . ftv108-term-paper\.bib)
-     (org-ref-pdf-directory concat
-			    (file-name-directory
-			     (buffer-file-name))
-			    "research")
-     (org-latex-hyperref-template)
-     (eval sh-set-shell "zsh")))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F"))))
- '(spaceline-highlight-face ((t (:background "#5F7F5F" :foreground "#1F1E20" :inherit (quote mode-line))))))
+)
+
