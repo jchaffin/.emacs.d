c2c7f5d08163f0a6572140a5c4c2238bf280de10
Jacob Chaffin <jchaffin@ucla.edu>
2018-01-04 03:22:42 -0800

    Add hook for loading per-project header files
    - Add aws.el package for limited aws-cli support
    - Vent out troubles with macOS High Sierra and llvm installation
      + clang-c library is a dependency for irony
    - Fix bug in compojure `defroute' indentation
    - Disable clojure error buffer
    - Add js-comint package for spawning/loading file buffers into
      inferior Node processes
      + Seems like comint prompt for package is _not_ read only

--- custom/custom-macos.el
+++ custom/custom-macos.el
@@ -4,11 +4,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(safe-local-variable-values
+   (quote
+    ((eval c-set-offset
+           (quote innamespace)
+           0)
+     (org-image-actual-width)
+     (cider-cljs-lein-repl . "(do (user/go) (user/cljs-repl))")
+     (cider-refresh-after-fn . "reloaded.repl/resume")
+     (cider-refresh-before-fn . "reloaded.repl/suspend")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
 
