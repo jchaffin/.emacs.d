e2bd26637d1c4005b55243cf5e4bb777155ce849
Jacob Chaffin <jchaffin@ucla.edu>
2018-02-01 15:50:19 -0800

    Add ASM configuration
    - Modifies =org-ref= file path variables
    - Adds interactive prompt for setting =org-latex-pdf-process= via the
      =set-org-latex-pdf-process= command

--- custom/custom-macos.el
+++ custom/custom-macos.el
@@ -1,6 +1,5 @@
 (setq org-agenda-files '("~/Dropbox/org/todos/TODOs.org"
                          "~/Dropbox/Documents/Courses/COMSCI_33/org/comsci33.org"
-                         "~/Dropbox/Documents/Courses/MATH_33A/org/math33a.org"
                          "~/Dropbox/Documents/Courses/LING_120B/org/ling120b.org"
                          "~/Dropbox/Documents/Courses/COMSCI_35L/org/comsci35L.org"))
 (custom-set-variables
@@ -10,7 +9,14 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    (quote
-    ((eval c-set-offset
+    ((org-ref-default-bibliography . "/Users/jacobchaffin/Dropbox/Documents/Courses/LING_120B/bib/ling120b.bib")
+     (org-ref-default-bibliography . /Users/jacobchaffin/Dropbox/Documents/Courses/LING_120B/bib/ling120b\.bib)
+     (org-ref-default-bibliography \`
+                                   (concat default-directory "bib/ling120b.bib"))
+     (org-ref-default-bibliography concat default-directory "bib/ling120b.bib")
+     (org-ref-default-bibliography . \./bib/ling120b\.bib)
+     (org-ref-default-bibliography . bib/ling120b\.bib)
+     (eval c-set-offset
            (quote innamespace)
            0)
      (org-image-actual-width)
