a240f95e73462c2d5dfc3157fb8991dfdd47d433
Jacob Chaffin <jchaffin@ucla.edu>
2018-06-30 04:11:46 -0700

    Fix org-mode faces

--- custom/custom-macos.el
+++ custom/custom-macos.el
@@ -170,5 +170,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:foreground "#65737e")))))
+)
 
