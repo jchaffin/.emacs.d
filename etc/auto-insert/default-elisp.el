266d46111e6baa78bdd391f462490e043281722b
Jacob Chaffin <jchaffin@ucla.edu>
2019-03-07 04:12:17 -0800

    Improve auto insert header for elisp

--- etc/auto-insert/default-elisp.el
+++ etc/auto-insert/default-elisp.el
@@ -1,14 +1,31 @@
-;;; `(file-name-nondirectory (buffer-file-name))` -- $0
-;;
-;; Author: `user-full-name` <`user-mail-address`>
+;;; `(file-name-nondirectory (buffer-file-name))` --- $0 -*- coding: utf-8; lexical-binding: t -*-
+
 ;; Copyright © `(format-time-string "%Y")`, `user-full-name`, all rights reserved.
+
+;; Version: 0.0.1
+;; Author: `user-full-name` -- `(format "%s" (concat "<" user-mail-address ">"))`
+;; URL: `(concat "https://github.com/" (shell-command-to-string "git config --global --get github.user") "/" (f-relative (f-dirname (buffer-file-name)) (f-dirname (f-dirname (buffer-file-name)))))`
 ;; Created: `(format-time-string "%e %B %Y")`
-;;
+;; Keywords: $2
+;; Package-Requires ((emacs "24.3"))
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
 ;;; Commentary:
 ;;
-;;  $2
+;;  $3
 ;;
 ;;; Code:
 
 $1
+
+(provide '`(file-name-sans-extension (file-name-nondirectory (buffer-file-name)))`)
 ;;; `(file-name-nondirectory (buffer-file-name))` ends here
