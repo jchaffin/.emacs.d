043108814db20b39c45634269f859a4f11746c66
Jacob Chaffin <jchaffin@ucla.edu>
2019-03-30 02:56:15 -0700

    Don't overcomplicate commentary

--- early-init.el
+++ early-init.el
@@ -1,25 +1,9 @@
-;;; -*- lexical-binding: t ; -*-
-;;; early-init.el -- Emacs early initialization file
+;;; early-init.el --- Emacs early initialization file  -*- lexical-binding: t ; -*-
 ;;
 ;; Copyright (c) 2018 Jacob Chaffin
 ;; Author Jacob Chaffin
 ;;
 ;; Homepage: https://github.com/.emacs.d.git
-;;
-;; Commentary:
-;;
-;; From the Emacs 27.1 NEWS:
-;;
-;;
-;;    Emacs can now be configured using an early init file.
-;;    The file is called 'early-init.el', in 'user-emacs-directory'.  It is
-;;    loaded very early in the startup process: before graphical elements
-;;    such as the tool bar are initialized, and before the package manager
-;;    is initialized.  The primary purpose is to allow customizing how the
-;;    package system is initialized given that initialization now happens
-;;    before loading the regular init file (see below).
-;;    PKG_COMMENTARY
-;;
 ;;
 ;;; Code:
 
