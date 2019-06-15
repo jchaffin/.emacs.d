5fe7c22d9862b0233899e26eb8eeb325dadd9b19
Jacob Chaffin <jchaffin@ucla.edu>
2019-06-15 01:55:04 -0700

    Add yarn project type

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;; init.el -- User initialization file for GNU Emacs
 ;;
-;;; Copyright (c) 2019 Jacob Chaffin:
+
+;; Copyright (c) 2019 Jacob Chaffin:
 ;;
 ;; Author: Jacob Chaffin <jchaffin@ucla.edu>
 ;; Keywords: emacs, .emacs.d, elisp, straight-el
@@ -23,29 +24,28 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
-(unwind-protect
     (let ((straight-treat-as-init t))
       (when (locate-library "gnutls")
         (require 'gnutls)
         ;; Prevent elpa from loading `package.el' in case loading fails.
         ;; Use LibreSSL certificates to bootstrap dependencies.
         (add-to-list 'gnutls-trustfiles "/usr/local/etc/libressl/cert.pem"))
-;;;; straight
-;;;;; configure straight
+;;; straight
+;;;; Variables
       (setq straight-repository-branch "develop"
             straight-profiles '((dotemacs . "versions.el")
                                 (nil . "default.el"))
             straight-current-profile 'dotemacs)
       ;; Enable `straight-live-modifications-mode' if its dependencies are
       ;; found.
-;;;;; straight live modifications:
+;;;; straight live modifications:
       (if (and (executable-find "watchexec")
                (executable-find "python3"))
           (setq straight-check-for-modifications
                 '(watch-files find-when-checking))
         (setq straight-check-for-modifications
               '(check-on-save find-when-checking)))
-;;;;; Bootstrap straight.el:
+;;;; Bootstrap straight.el:
       (let ((bootstrap-file
              (expand-file-name
               "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -54,10 +54,6 @@
             (repo "raxod502/straight.el")
             (branch straight-repository-branch)
             (remote-file "install.el"))
-        ;; TODO Add error handling if straight fails to install or load.
-        ;; If anything goes wrong here it won't throw until the unwind form
-        ;; at the end of this file when `straight-finalize-transaction'
-        ;; isn't defined.
         (unless (file-exists-p bootstrap-file)
           (with-current-buffer
               (url-retrieve-synchronously
@@ -102,6 +98,7 @@
               `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
 
       (customize-set-variable 'load-prefer-newer t) ;; load newer bytecode
+;;;; auto compile
       (use-package auto-compile
         :demand t
         :init
@@ -111,7 +108,7 @@
       (require 'subr-x)
       (straight-use-package 'git)
 
-;;;; Org Mode:
+;;; Org
 ;;;;; install org mode:
       (straight-use-package 'org-plus-contrib)
 ;;;;; Org configuration
@@ -123,7 +120,7 @@
         (org-directory (file-truename "~/Dropbox/org"))
         ;; setup archive directory in current folder
         (org-archive-location "archive/%s_archive::")
-;;;;;;; Org src
+;;;;;;; Org source
 	      (org-confirm-babel-evaluate nil)
         (org-src-fontify-natively t)
         (org-src-tab-acts-natively t)
@@ -133,6 +130,7 @@
         (org-ctrl-k-protect-subtree 'error)
         (org-catch-invisible-edits 'smart)
 ;;;;;;; Structure and Appearance
+        (org-insert-heading-respect-content t)
         (org-ellipsis "")
         (org-list-allow-alphabetical t)
         (org-hide-emphasis-markers t)
@@ -143,6 +141,7 @@
         (org-blank-before-new-entry
          '((heading . auto)
            (plain-list-item . auto)))
+;;;;;;; org libraries
         (org-modules '(org-bbdb
                        org-bibtex
                        org-crypt
@@ -163,6 +162,7 @@
                        ol-bookmark
                        ol-man
                        ol-elisp-symbol))
+;;;;;; org keybindings
         :bind
         (("C-c a" . org-agenda)
          ("C-c c" . org-capture)
@@ -170,8 +170,8 @@
          (:map org-mode-map
                ("C-c C-x h" . org-toggle-link-display)
                ("C-c C-s" . org-schedule))))
-;;;; Literate
-;;;;; Custom variables
+;;; Literate
+;;;; Custom variables
       (defgroup dotemacs nil
         "Customization group for the `dotemacs' Emacs configuration."
         :group 'applications
@@ -194,7 +194,7 @@
           "core/rainbow-delimiters"
           "core/swiper")
         "Named source blocks to tangle when `use-literate-p' is enabled. ")
-;;;;; tangle and load
+;;;; tangle and load
       ;; Extract source code and load the config
       (if use-literate-p
           (if (file-exists-p dotemacs-literate-config-file)
@@ -202,5 +202,4 @@
             (error "File does not exist %s" dotemacs-literate-config-file))
         (mapcar #'literate-tangle-src-block literate-debug-blocks))
       (setq initial-buffer-choice dotemacs-literate-config-file))
-  (straight-finalize-transaction))
 ;;; init.el ends here
