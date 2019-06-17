7f993dd3ea7bc42263ceed0ae5088a48da981f2d
Jacob Chaffin <jchaffin@ucla.edu>
2019-06-17 10:53:03 -0700

    Add which key replacements

---
+++ .cask/27.0/bootstrap/dash-20190424.1804/dash-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jacobchaffin/.emacs.d/.cask/27.0/bootstrap/dash-20190424.1804/dash.el  -*- no-byte-compile: t -*-
+(define-package "dash" "20190424.1804" "A modern list library for Emacs" 'nil :commit "77f3bf40c9c85386a50f2dab3dc950513f6f88bd" :keywords '("lists") :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com"))
